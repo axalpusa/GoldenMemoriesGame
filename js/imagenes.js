ff1bed8ec49d70685a33fc2354b0be9efeb5556f
axelpuertas <axalpusa1125@gmail.com>
2024-11-14 11:38:01 -0500

    init game memories

---
+++ js/imagenes.js
@@ -0,0 +1,63 @@
+let nivelUno = [];
+let nivelDos = [];
+let nivelTres = [];
+let reverso = "img/reverso.png";
+
+function cargarImagenes() {
+
+	nivelUno[0] = "img/nivelUno/imagen1.png";
+	nivelUno[1] = "img/nivelUno/imagen2.png";
+	nivelUno[2] = "img/nivelUno/imagen3.png";
+	nivelUno[3] = "img/nivelUno/imagen4.png";
+	nivelUno[4] = "img/nivelUno/imagen5.png";
+	nivelUno[5] = "img/nivelUno/imagen6.png";
+	nivelUno[6] = "img/nivelUno/imagen7.png";
+	nivelUno[7] = "img/nivelUno/imagen8.png";
+	nivelUno[8] = "img/nivelUno/imagen9.png";
+	nivelUno[9] = "img/nivelUno/imagen10.png";
+
+	nivelDos[1] = "img/nivelDos/imagen1.png";
+	nivelDos[0] = "img/nivelDos/imagen2.png";
+	nivelDos[2] = "img/nivelDos/imagen3.png";
+	nivelDos[3] = "img/nivelDos/imagen4.png";
+	nivelDos[4] = "img/nivelDos/imagen5.png";
+	nivelDos[5] = "img/nivelDos/imagen6.png";
+	nivelDos[6] = "img/nivelDos/imagen7.png";
+	nivelDos[7] = "img/nivelDos/imagen8.png";
+	nivelDos[8] = "img/nivelDos/imagen9.png";
+	nivelDos[9] = "img/nivelDos/imagen10.png";
+
+	nivelTres[0] = "img/nivelTres/imagen1.png";
+	nivelTres[1] = "img/nivelTres/imagen2.png";
+	nivelTres[2] = "img/nivelTres/imagen3.png";
+	nivelTres[3] = "img/nivelTres/imagen4.png";
+	nivelTres[4] = "img/nivelTres/imagen5.png";
+	nivelTres[5] = "img/nivelTres/imagen6.png";
+	nivelTres[6] = "img/nivelTres/imagen7.png";
+	nivelTres[7] = "img/nivelTres/imagen8.png";
+	nivelTres[8] = "img/nivelTres/imagen9.png";
+	nivelTres[9] = "img/nivelTres/imagen10.png";
+	nivelTres[10] = "img/nivelTres/imagen11.png";
+	nivelTres[11] = "img/nivelTres/imagen12.png";
+	nivelTres[12] = "img/nivelTres/imagen13.png";
+	nivelTres[13] = "img/nivelTres/imagen14.png";
+	nivelTres[14] = "img/nivelTres/imagen15.png";
+	nivelTres[15] = "img/nivelTres/imagen16.png";
+	nivelTres[16] = "img/nivelTres/imagen17.png";
+	nivelTres[17] = "img/nivelTres/imagen18.png";
+	nivelTres[18] = "img/nivelTres/imagen19.png";
+	nivelTres[19] = "img/nivelTres/imagen20.png";	
+	nivelTres[20] = "img/nivelTres/imagen21.png";
+	nivelTres[21] = "img/nivelTres/imagen22.png";
+	nivelTres[22] = "img/nivelTres/imagen23.png";
+	nivelTres[23] = "img/nivelTres/imagen24.png";
+	nivelTres[24] = "img/nivelTres/imagen25.png";
+	nivelTres[25] = "img/nivelTres/imagen26.png";
+	nivelTres[26] = "img/nivelTres/imagen27.png";
+	nivelTres[27] = "img/nivelTres/imagen28.png";
+	nivelTres[28] = "img/nivelTres/imagen29.png";
+	nivelTres[29] = "img/nivelTres/imagen30.png";
+	nivelTres[30] = "img/nivelTres/imagen31.png";
+	nivelTres[31] = "img/nivelTres/imagen32.png";
+
+}
